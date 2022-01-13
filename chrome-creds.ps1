a7b217b3a75b44c53f0c04fed4bf94e8fec061af
Marco Simioni <MARCOSIM@ie.ibm.com>
2022-01-13 11:17:47 +0000

    remove unneeded dependency

--- chrome-creds.ps1
+++ chrome-creds.ps1
@@ -26,7 +26,7 @@
    WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
    #>
 
-#requires -modules getsql -version 7
+#requires -version 7
 
 using namespace "System.Security.Cryptography"
 param (
